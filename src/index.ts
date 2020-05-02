4a72cf83b485b4ae0c52f30dfa22057e6f65f829
maucapuchin <maucap12m@gmail.com>
2020-05-02 12:54:51 -0500

    cambios

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,14 @@ import userRoutes from './routes/user.routes'
 
 const body_parser = require('body-parser');
 const app = express()
-app.use(body_parser.urlencoded({extended:true}));
 createConnection();
 
 //middlewares
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
+/* middleware para coger datos por body */
+app.use(body_parser.urlencoded({extended:true}));
 
 //routes
 app.use(userRoutes)
